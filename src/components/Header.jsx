43e0536d42c07161e74099be969d68ca6aac7474
redexeduniox <rahulonered@gmail.com>
2024-02-06 04:45:44 -0800

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import logoURL from "../../public/tesla.svg";
+
+export default function Header() {
+	return (
+		<>
+			<header className="">
+				Build the Header & Nav Element here
+				{/* Write the code for the header element */}
+			</header>
+		</>
+	);
+}
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+	theme: {
+		fontFamily: {
+			sans: ["'Copyright (C) H&Co typography.com'", "sans-serif"],
+		},
+	},
+	plugins: [],
+};
